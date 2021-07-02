c4bd08fabf90b2a7798933bf8ac09832bf8c29e3
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-07-02 16:12:01 +0800

    chore: delete db addr log

--- initialize/gorm.go
+++ initialize/gorm.go
@@ -1,7 +1,6 @@
 package initialize
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"time"
@@ -16,7 +15,6 @@ import (
 func InitMySQL() {
 	addr, username, password, database := "rm-uf6ji600qianqe6921o.mysql.rds.aliyuncs.com:3306", "buaase2021", "buaase(2021)", "durian"
 	dsn := username + ":" + password + "@tcp(" + addr + ")/" + database + "?charset=utf8mb4&parseTime=True&loc=Local"
-	fmt.Println(dsn)
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
